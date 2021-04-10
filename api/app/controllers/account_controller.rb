887fc01d6dac42a5a549022474bc04336c9741d7
Lenny Andreu <landreuhs.29@gmail.com>
2021-04-09 22:07:42 -0400

    Add login method on account, informs if access is granted or not

--- api/app/controllers/account_controller.rb
+++ api/app/controllers/account_controller.rb
@@ -1,4 +1,13 @@
 class AccountController < ApplicationController
+
   def login
+    account = Account.find_by(account_params)
+
+    render json: { message: account != nil ? "Access granted" : "Invalid credentials" , status: account != nil }
   end
+
+  private 
+    def account_params
+      params.require(:account).permit(:username, :password)
+    end
 end
