eeca70016427a5b542fd94bd8fd31637873e95cf
Lenny Andreu <landreuhs.29@gmail.com>
2021-04-09 22:43:39 -0400

    Add CRUD to products

--- api/app/controllers/products_controller.rb
+++ api/app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
 
+  #include AuthenticationConcern
+
   def get
     if params[:id] != nil and product = Product.find(params[:id])
       render json: product
@@ -9,12 +11,36 @@ class ProductsController < ApplicationController
   end
 
   def post
+    product = Product.new(products_params)
+
+    if product.save 
+      render json: { message: 'OK', product: product}
+    else
+      render json: { message: 'Unable to save'}
+    end
   end
 
   def put
+    product = Product.find(params[:id])
+
+    if product.update(products_params)
+      render json: { message: 'OK', product: product}
+    else
+      render json: { message: 'Unable to update'}
+    end
   end
 
   def delete
+    product = Product.find(params[:id])
+    if product.destroy
+      render json: { message: 'OK', product: product}
+    else
+      render json: { message: 'Unable to delete'}
+    end
   end
 
+  private 
+    def products_params
+      params.require(:product).permit(:code, :name, :brand, :ammount, :price, :category_id)
+    end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'products', to: 'products#get'
+  get 'products/:id', to: 'products#get'
   post 'products', to: 'products#post'
-  put 'products/put'
-  get 'products/delete'
+  put 'products', to: 'products#put'
+  delete 'products/delete', to: 'products#delete'
   post 'login', to: 'account#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
